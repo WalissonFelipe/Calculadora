a2e35744c7f95e1681e37d4d1efa30f1632eef14
Walisson Felipe <Allysson_felipe@hotmail.com>
2016-05-24 02:36:26 -0400

    Tela Splash e Icone do aplicativo

---
+++ Calculadora/app/src/main/java/com/example/mint/calculadora/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.mint.calculadora;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int TempoSplash = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent tela = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(tela);
+                finish();
+            }
+        }, TempoSplash);
+    }
+}
